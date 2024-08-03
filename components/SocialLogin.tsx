24415aac30f0b9364b2b2c773fb03c2ec501b5c6
GCgang <powm97@gmail.com>
2024-08-03 20:34:59 +0900

    Feat: CSS-SocialLogin

--- components/SocialLogin.tsx
+++ components/SocialLogin.tsx
@@ -5,13 +5,19 @@ import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/20/solid';
 export default function SocialLogin() {
   return (
     <>
-      <div />
-      <div>
-        <Link href='/github/start'>
+      <div className='w-full h-px bg-neutral-500' />
+      <div className='flex flex-col gap-3'>
+        <Link
+          href='/github/start'
+          className='primary-btn flex h-10 items-center justify-center gap-2'
+        >
           <FaGithub className='size-6' />
           <span>Continue with Github</span>
         </Link>
-        <Link href='/sms'>
+        <Link
+          href='/sms'
+          className='primary-btn flex h-10 items-center justify-center gap-2'
+        >
           <ChatBubbleOvalLeftEllipsisIcon className='size-6' />
           <span>Continue with SMS</span>
         </Link>
