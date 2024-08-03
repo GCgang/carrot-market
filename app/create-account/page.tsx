ea8d42f7d5279f553b5a280c79963b638e31dc77
GCgang <powm97@gmail.com>
2024-08-03 20:34:00 +0900

    Feat: CSS-CreateAccount

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -4,13 +4,12 @@ import SocialLogin from '@/components/SocialLogin';
 
 export default function CreateAccount() {
   return (
-    <div>
-      <div>
-        <h1>안녕하세요</h1>
-        <h2>Fill in the form below to join</h2>
+    <div className='flex flex-col gap-10 py-8 px-6'>
+      <div className='flex flex-col gap-2 *:font-medium'>
+        <h1 className='text-2xl'>안녕하세요</h1>
+        <h2 className='text-xl'>Fill in the form below to join</h2>
       </div>
-
-      <form>
+      <form className='flex flex-col gap-3'>
         <FormInput type='text' placeholder='Username' required errors={[]} />
         <FormInput type='email' placeholder='Email' required errors={[]} />
         <FormInput
