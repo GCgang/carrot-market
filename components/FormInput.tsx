040cecc2e5f63b98eeea091693c80a1e05f8b720
GCgang <powm97@gmail.com>
2024-08-03 20:34:47 +0900

    Feat: CSS-FormInput

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -12,10 +12,17 @@ export default function FormInput({
   errors,
 }: FormInputProps) {
   return (
-    <div>
-      <input type={type} placeholder={placeholder} required={required} />
+    <div className='flex flex-col gap-2'>
+      <input
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        className='bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400'
+      />
       {errors.map((error, index) => (
-        <span key={index}>{error}</span>
+        <span key={index} className='text-red-500 font-medium'>
+          {error}
+        </span>
       ))}
     </div>
   );
