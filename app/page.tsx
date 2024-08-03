d49b24bfe5532960618c84129bc614455eeaef9c
GCgang <powm97@gmail.com>
2024-08-03 19:25:39 +0900

    Feat: Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div>
+      <div>
+        <span>🥕</span>
+        <h1>당근</h1>
+        <h2>당근 마켓</h2>
+      </div>
+      <div>
+        <Link href='/create-account'>시작하기</Link>
+        <div>
+          <span>이미 계정이 있으신가요?</span>
+          <Link href='/login'>로그인</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
