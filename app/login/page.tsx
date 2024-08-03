c7bd4b077000b19812e4c368ac665d05fe2adb28
GCgang <powm97@gmail.com>
2024-08-03 20:34:08 +0900

    Feat: CSS-Login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,12 +4,12 @@ import SocialLogin from '@/components/SocialLogin';
 
 export default function Login() {
   return (
-    <div>
-      <div>
-        <h1>안녕하세요</h1>
-        <h2>Log in with email and password</h2>
+    <div className='flex flex-col gap-10 py-8 px-6'>
+      <div className='flex flex-col gap-2 *:font-medium'>
+        <h1 className='text-2xl'>안녕하세요</h1>
+        <h2 className='text-xl'>Log in with email and password</h2>
       </div>
-      <form>
+      <form className='flex flex-col gap-3'>
         <FormInput type='email' placeholder='Email' required errors={[]} />
         <FormInput
           type='password'
