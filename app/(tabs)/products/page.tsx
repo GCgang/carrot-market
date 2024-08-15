2357d0c1315c09e8e4fa398ac7929f2d9d93002c
GCgang <powm97@gmail.com>
2024-08-15 18:24:32 +0900

    Feat: product list

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,9 @@
 import ListProduct from '@/components/ListProduct';
+import ProductList from '@/components/ProductList';
 import db from '@/lib/db';
+import { Prisma } from '@prisma/client';
 
-async function getProducts() {
+async function getInitialProducts() {
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -10,18 +12,23 @@ async function getProducts() {
       photo: true,
       id: true,
     },
+    take: 1,
+    orderBy: {
+      created_at: 'desc',
+    },
   });
   return products;
 }
 
+export type InitialProducts = Prisma.PromiseReturnType<
+  typeof getInitialProducts
+>;
+
 export default async function Products() {
-  const products = await getProducts();
+  const initialProducts = await getInitialProducts();
   return (
     <div>
-      <h1 className='text-white text-4xl'>Products!</h1>
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product}></ListProduct>
-      ))}
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
---
+++ components/ProductList.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { InitialProducts } from '@/app/(tabs)/products/page';
+import { useState } from 'react';
+import ListProduct from './ListProduct';
+import getMoreProducts from '@/app/(tabs)/products/action';
+
+interface IProductListProps {
+  initialProducts: InitialProducts;
+}
+
+export default function ProductList({ initialProducts }: IProductListProps) {
+  const [products, setProducts] = useState(initialProducts);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const handleLoadMore = async () => {
+    setIsLoading(true);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
+    setIsLoading(false);
+  };
+  return (
+    <div>
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+      {isLastPage ? null : (
+        <button onClick={handleLoadMore} disabled={isLoading}>
+          {isLoading ? '로딩 중' : 'Load more'}
+        </button>
+      )}
+    </div>
+  );
+}
