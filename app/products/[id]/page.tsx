2821e6645dc4066cbe42843d3a54c664c448b8fd
GCgang <powm97@gmail.com>
2024-08-18 19:59:32 +0900

    Refactor: product modal

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { formatToWon } from '@/lib/utils';
 import Link from 'next/link';
 
-async function getProduct(productId: number) {
+export async function getProduct(productId: number) {
   const product = db.product.findUnique({
     where: {
       id: productId,
