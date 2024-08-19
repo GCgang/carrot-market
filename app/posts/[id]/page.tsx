7120c683017d2c21882d67d469c56791bee2bb49
GCgang <powm97@gmail.com>
2024-08-19 17:52:54 +0900

    Refactor: useOptimistic 사용

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,11 +1,11 @@
 import db from '@/lib/db';
 import getSession from '@/lib/session';
 import { notFound } from 'next/navigation';
-import { EyeIcon, HandThumbUpIcon } from '@heroicons/react/24/solid';
-import { HandThumbUpIcon as OutlineHandThumbUpIcon } from '@heroicons/react/24/outline';
+import { EyeIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import { formatToTimeAgo } from '@/lib/utils';
-import { unstable_cache as nextCache, revalidateTag } from 'next/cache';
+import { unstable_cache as nextCache } from 'next/cache';
+import LikeButton from '@/components/LikeButton';
 
 async function getPost(id: number) {
   try {
@@ -84,34 +84,7 @@ export default async function PostDetail({
   if (!post) {
     return notFound();
   }
-  const likePost = async () => {
-    'use server';
-    const session = await getSession();
-    try {
-      await db.like.create({
-        data: {
-          postId: id,
-          userId: session.id!,
-        },
-      });
-      revalidateTag(`like-status-${id}`);
-    } catch (e) {}
-  };
-  const dislikePost = async () => {
-    'use server';
-    try {
-      const session = await getSession();
-      await db.like.delete({
-        where: {
-          id: {
-            postId: id,
-            userId: session.id!,
-          },
-        },
-      });
-      revalidateTag(`like-status-${id}`);
-    } catch (e) {}
-  };
+
   const { likeCount, isLiked } = await getCachedLikeStatus(id);
   return (
     <div className='p-5 text-white'>
@@ -137,26 +110,7 @@ export default async function PostDetail({
           <EyeIcon className='size-5' />
           <span>조회 {post.views}</span>
         </div>
-        <form action={isLiked ? dislikePost : likePost}>
-          <button
-            className={`flex items-center gap-2 text-neutral-400 text-sm border border-neutral-400 rounded-full p-2  transition-colors ${
-              isLiked
-                ? 'bg-orange-500 text-white border-orange-500'
-                : 'hover:bg-neutral-800'
-            }`}
-          >
-            {isLiked ? (
-              <HandThumbUpIcon className='size-5' />
-            ) : (
-              <OutlineHandThumbUpIcon className='size-5' />
-            )}
-            {isLiked ? (
-              <span> {likeCount}</span>
-            ) : (
-              <span>공감하기 ({likeCount})</span>
-            )}
-          </button>
-        </form>
+        <LikeButton isLiked={isLiked} likeCount={likeCount} postId={id} />
       </div>
     </div>
   );
